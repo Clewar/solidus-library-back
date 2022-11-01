e4e0b97a97b78fe66b6691fb46a0518d608fba91
clewar <isaimonza99@gmail.com>
2022-10-31 18:02:43 -0700

    back v2 done

--- src/books/dto/update-book.dto.ts
+++ src/books/dto/update-book.dto.ts
@@ -1,8 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateBookDto } from './create-book.dto';
-import { IsNotEmpty } from 'class-validator';
 
-export class UpdateBookDto extends PartialType(CreateBookDto) {
-  @IsNotEmpty()
-  id: number;
-}
+export class UpdateBookDto extends PartialType(CreateBookDto) {}
